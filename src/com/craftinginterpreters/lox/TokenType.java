7e82f922f662f5bb058bf729a2fb8d73d5857303
Jhonathan Salguero Villa <sney2002@gmail.com>
2020-01-02 12:15:51 -0500

    Implement scanner (chapter 4)

---
+++ src/com/craftinginterpreters/lox/TokenType.java
@@ -0,0 +1,25 @@
+package com.craftinginterpreters.lox;
+
+/**
+ * Created by sney2002 on 2/01/20.
+ */
+public enum TokenType {
+    // Single character tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
