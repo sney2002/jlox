b6a10c9464c6593b37ad62ec7c77f0d3e2828fa7
Jhonathan Salguero Villa <sney2002@gmail.com>
2020-04-11 09:05:59 -0500

    Add expression printers

---
+++ src/com/craftinginterpreters/lox/RPNPrinter.java
@@ -0,0 +1,60 @@
+package com.craftinginterpreters.lox;
+
+public class RPNPrinter implements Expr.Visitor<String> {
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return rnpPrint(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return expr.expression.accept(this);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return expr.operator.lexeme + expr.right.accept(this);
+    }
+
+    private String rnpPrint(String name, Expr... exprs) {
+        StringBuilder builder = new StringBuilder();
+
+        for (Expr expr : exprs) {
+            builder.append(expr.accept(this));
+            builder.append(" ");
+        }
+
+        if (! name.isEmpty()) {
+            builder.append(name);
+        }
+
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        Expr expression = new Expr.Binary(
+                new Expr.Binary(
+                        new Expr.Literal(1),
+                        new Token(TokenType.PLUS, "+", null, 1),
+                        new Expr.Literal(2)
+                ),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Binary(
+                        new Expr.Literal(4),
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(3)
+                )
+        );
+
+        System.out.println(new RPNPrinter().print(expression));
+    }
+}
