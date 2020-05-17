4696c7d4fc1854550b955637f6dd4e6383498f54
Jhonathan Salguero Villa <sney2002@gmail.com>
2020-05-16 22:03:48 -0500

    Add LoxCallable interface

--- src/com/craftinginterpreters/lox/LoxCallable.java
+++ src/com/craftinginterpreters/lox/LoxCallable.java
@@ -1,4 +1,9 @@
 package com.craftinginterpreters.lox;
 
-public class LoxCallable {
+import java.util.List;
+
+interface LoxCallable {
+    public int arity();
+
+    public Object call(Interpreter interpreter, List<Object> params);
 }
